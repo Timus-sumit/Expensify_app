97f209b75f3b4dccdb63afe1d25bbbc8699833c3
sumit <ss2637944@gmail.com>
2021-03-10 14:12:21 +0530

    Remove dummy data

--- src/app.js
+++ src/app.js
@@ -10,13 +10,8 @@ import './styles/styles.scss'
 
 const store = configureStore();
 
-store.dispatch(addExpense({description:'water bill', amount : '200'}));
-store.dispatch(addExpense({description: 'gas bill', amount:'300'}));
-store.dispatch(addExpense({description: 'rent', amount:'10000'}));
 
-// store.dispatch(setTextFilter('bill'))
-// store.dispatch(sortByAmount());
-// console.log(store.getState());
+
 
 const jsx = (
     <Provider store={store}>
