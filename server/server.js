d6a9623bdb76057d21bbb71598c4c0a1e7209b5e
sumit <ss2637944@gmail.com>
2021-03-10 22:42:01 +0530

    Setup production build and server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PublicPath = path.join(__dirname,'..','public');
+const port = process.env.PORT || 3000
+
+app.use(express.static(PublicPath))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(PublicPath,'index.html'));
+})
+
+app.listen(port,()=>{
+    console.log('server is up!')
+})
